fa3f0e1cbf63225fb48796bb07d6e1eeb212e956
Lenkaa3792 <allanlenkaa@gmail.com>
2022-07-07 12:36:21 +0300

    updates

---
+++ index.js
@@ -0,0 +1,13 @@
+//alert('welcome')
+var btn = document.getElementById( '+1' )
+var btn2 = document.getElementById( 'count' )
+// btn2.innerText = '7'
+// btn.innerText = "7";
+currentbtn2 = 0;
+btn.addEventListener( 'click', () =>
+{
+    
+    currentbtn2++
+    count.innerText=currentbtn2
+    
+})
