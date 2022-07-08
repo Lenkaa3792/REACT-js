e314f0c9a8b8fab718580539bf1d19bcbe005c20
Lenkaa3792 <allanlenkaa@gmail.com>
2022-07-08 15:06:43 +0300

    update

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-//alert('welcome')
-var btn = document.getElementById( '+1' )
-var btn2 = document.getElementById( 'count' )
-// btn2.innerText = '7'
-// btn.innerText = "7";
-currentbtn2 = 0;
-btn.addEventListener( 'click', () =>
-{
+
+// //alert('welcome')
+// var btn = document.getElementById( '+1' )
+// var btn2 = document.getElementById( 'count' )
+// document.getElementById('plus')
+// // btn2.innerText = '7'
+// // btn.innerText = "7";
+// currentbtn2 = 9;
+// initialplus = 0;
+// btn.addEventListener( 'click', () =>
+// {
     
-    currentbtn2++
-    count.innerText=currentbtn2
+//     currentbtn2++
+//     initialplus++
+//     count.innerText = currentbtn2
+//     plus.innerText=initialplus
     
-})
+// })
+const contRoot=document.getElementById( 'root' )
+ReactDOM.render( 'Hello  World!', contRoot )
+const rootTwo = document.getElementById( 'root2' )
+ReactDOM.render('Second line of React js!!',rootTwo)
